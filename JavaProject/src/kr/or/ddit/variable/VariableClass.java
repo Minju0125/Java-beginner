b1dd54d58c833f3d4869579d3dcc4194c953551a
DESKTOP-DRNL4PU\PC-13 <minju.park.kim@gmail.com>
2023-05-17 21:12:18 +0900

    담당선생님 보강

---
+++ JavaProject/src/kr/or/ddit/variable/VariableClass.java
@@ -0,0 +1,20 @@
+package kr.or.ddit.variable;
+
+public class VariableClass {
+	/* 2. 변수와 타입
+	 *  -변수 선언 및 초기화
+	 *  -변수 명명 규칙
+	 *  -기본 타입
+	 *  -타입 변환
+	 *	  -변수와 시스템 입출력
+	 * 
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		/*
+		 * 1) 변수 선언 및 초기화
+		 * 
+		 */
+	}
+}
